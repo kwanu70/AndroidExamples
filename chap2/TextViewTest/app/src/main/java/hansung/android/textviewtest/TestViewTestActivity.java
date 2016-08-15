a29f532bc7b4113e780d7adcf3fda47d4edb96d0
kwanwoo <kwanu.lee@gmail.com>
2016-08-15 17:25:47 +0900

    chap2/TextViewTest first version

---
+++ chap2/TextViewTest/app/src/main/java/hansung/android/textviewtest/TestViewTestActivity.java
@@ -0,0 +1,13 @@
+package hansung.android.textviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestViewTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_view_test);
+    }
+}
