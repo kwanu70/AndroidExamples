ea1057bd259315ece4e64149108233ec9bd822fd
KW-OFFICE\Kwanwoo <kwlee@hansung.ac.kr>
2016-09-05 10:43:40 +0900

    GridView 초기 버전

---
+++ chap3/HelloGridView/app/src/main/java/com/example/kwanwoo/hellogridview/HelloGridViewActivity.java
@@ -0,0 +1,33 @@
+package com.example.kwanwoo.hellogridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.view.View;
+
+public class HelloGridViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_grid_view);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(HelloGridViewActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
+
+
+
