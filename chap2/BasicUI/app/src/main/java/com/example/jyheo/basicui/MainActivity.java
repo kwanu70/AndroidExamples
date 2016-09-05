a75a101b20f8feb1432c345be87cb2bd9be32278
KW-OFFICE\Kwanwoo <kwlee@hansung.ac.kr>
2016-09-05 10:24:57 +0900

    BasicUI 이벤트 처리 방법 수정

--- chap2/BasicUI/app/src/main/java/com/example/jyheo/basicui/MainActivity.java
+++ chap2/BasicUI/app/src/main/java/com/example/jyheo/basicui/MainActivity.java
@@ -13,20 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear_layout);
 
-        Button btn = (Button)findViewById(R.id.button4);
+        Button btn = (Button)findViewById(R.id.button1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Button btn = (Button)findViewById(R.id.button1);
-                if (btn.getText().toString() == "Button 1")
-                    btn.setText("Button One");
-                else
-                    btn.setText("Button 1");
+                Toast.makeText(getApplicationContext(), R.string.button_clicked_msg,
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
-    public void b1OnClick(View v) {
+   /* public void b1OnClick(View v) {
         Toast.makeText(getApplicationContext(), R.string.button_clicked_msg,
                 Toast.LENGTH_SHORT).show();
     }
+    */
 }
