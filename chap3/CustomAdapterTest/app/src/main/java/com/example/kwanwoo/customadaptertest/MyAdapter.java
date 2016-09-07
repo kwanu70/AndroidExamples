2d426dae403af54fdc4091fc8b2b01f02dcfddc0
KW-OFFICE\Kwanwoo <kwlee@hansung.ac.kr>
2016-09-07 16:04:03 +0900

    chap3/CustomerView:  MyItem 위치 이동

--- chap3/CustomAdapterTest/app/src/main/java/com/example/kwanwoo/customadaptertest/MyAdapter.java
+++ chap3/CustomAdapterTest/app/src/main/java/com/example/kwanwoo/customadaptertest/MyAdapter.java
@@ -63,3 +63,16 @@ public class MyAdapter extends BaseAdapter {
         return convertView;
     }
 }
+
+class MyItem {
+    int mIcon; // image resource
+    String nName; // text
+    String nAge;  // text
+
+    MyItem(int aIcon, String aName, String aAge) {
+        mIcon = aIcon;
+        nName = aName;
+        nAge = aAge;
+    }
+}
+
