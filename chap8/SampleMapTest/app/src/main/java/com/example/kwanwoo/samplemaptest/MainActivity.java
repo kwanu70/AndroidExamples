6ad8aff15461f81b6cb239a093f13ed8977cc262
KW-OFFICE\Kwanwoo <kwlee@hansung.ac.kr>
2016-10-27 19:15:43 +0900

    chap8/SampleMapTest 초기 커밋

---
+++ chap8/SampleMapTest/app/src/main/java/com/example/kwanwoo/samplemaptest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.kwanwoo.samplemaptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
